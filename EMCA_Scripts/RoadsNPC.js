9657939a4fde5611ad170a0dcf6942eb0f1758b4
edwardg117 <8350193+edwardg117@users.noreply.github.com>
2020-01-18 20:36:57 +0800

    Fixed the script so now you call trekTo(npc, destination, speed) in order to start navigation. Should run better too. Cleaned up the code, looks nicer. (Could probably add more comments later)
    Signed-off-by: edwardg117 <8350193+edwardg117@users.noreply.github.com>

--- EMCA_Scripts/RoadsNPC.js
+++ EMCA_Scripts/RoadsNPC.js
@@ -5,19 +5,33 @@ Functions for the CNPC to use so it can use roads properly
 Companion script: Roads.js
 Uses jsonified by ratquaza (baito), https://github.com/ratquaza/jsonified
 
-In this version:
+About this script:
 - Can navigate from one node to another if nodes have been mapped/pathed (any node to any node)
 - Navigates to the starting node if it's not already standing on it
-- Stops navigating hen target node is reached
+- Stops navigating when target node is reached
 - Navigation speed can be changed from walking super slow to running at the speed of light
 - Attempts to autorecover if navigation fails (because NPCs seem to stop navigating even if they haven't reached the target location)
     - Tries to start pathfinding again 3 times
     - On the 4th try the NPC is teleported to the node it can't reach
 
 
+
+New in this version:
+- New functions:
+initializeRoadsNPC(event)
+    - Replaces the old init function and should be called in your own init (ONCE)
+
+getShortestPath(start, end, npc)
+    - Determines if the nodes can be reached and returns the shortest path
+
+trekTo(npc, destination, speed)
+    - Starts navigation
+    - This should be called to make the NPC navigate
+
+- Now uses timer better, less calculations
+
 Planned:
 - (I'm making this up as a go along)
-
 
 Put into NPC:
 var navSpeed = 2 // How fast the NPC should walk range(1-5)
@@ -36,169 +50,147 @@ Make Spider in config\CNPCsRoads\:
 var spiderClass = Java.type("org.baito.forge.jsonified.Spider")
 var spider = new spiderClass()
 spider.in("CNPCsRoads")
+
+Things to remember
+Timer values:
+1 = Check navigation, if need to move to the next node or try to recover
 */
 
-function init(event)
+function initializeRoadsNPC(event)
 {
-    event.npc.getStoreddata().put("NavSpeed", navSpeed)
-    event.npc.getStoreddata().put("isNavigating", 0)
-    event.npc.getStoreddata().put("Home", home)
-    event.npc.getStoreddata().put("HomeCity", city)
-    event.npc.getStoreddata().put("CurrentLocation", home)
-    event.npc.getStoreddata().put("NavTo", home)
-    event.npc.getStoreddata().put("navPath", "")
-    event.npc.getStoreddata().put("consecutiveRevoverFails", 0)
-
+    event.npc.getStoreddata().put("NavSpeed", navSpeed);
+    event.npc.getStoreddata().put("isNavigating", 0);
+    event.npc.getStoreddata().put("Home", home);
+    event.npc.getStoreddata().put("HomeCity", city);
+    event.npc.getStoreddata().put("CurrentLocation", home);
+    event.npc.getStoreddata().put("NavTo", home);
+    event.npc.getStoreddata().put("navPath", "");
+    event.npc.getStoreddata().put("consecutiveRevoverFails", 0);
 }
 
-function prepNav(npc, start, end, speed)
+function getShortestPath(start, end, npc)
 {
-    // Moves NPC to starting node and returns the shortest path
-    var spiderClass = Java.type("org.baito.forge.jsonified.Spider")
-    var spider = new spiderClass()
-    spider.in("CNPCsRoads")
-    var shortestPath = ""
+    // Returns the shortest path from start to end or null if not possible
+    var spiderClass = Java.type("org.baito.forge.jsonified.Spider");
+    var spider = new spiderClass();
+    spider.in("CNPCsRoads");
+    var shortestPath = null;
 
     if(spider.exists(npc.world.getName() + "_NodeRegistry.json")) // Does the Node Registry exist? Can't go anywhere without it
     {
-        if(spider.exists(npc.world.getName() + "_paths.json"))
+        if(spider.exists(npc.world.getName() + "_paths.json")) // Have paths been mapped? Need those to know where to go
         {
-            var nodeRegistry = JSON.parse(spider.get(npc.world.getName() + "_NodeRegistry.json"))
-            var paths = JSON.parse(spider.get(npc.world.getName() + "_paths.json"))
-            // Check to see if the node has already been created under that name
-            if(start in nodeRegistry)
+            // Node registry exists and Paths have been made, start looking for the path
+            var paths = JSON.parse(spider.get(npc.world.getName() + "_paths.json"));
+            var nodeRegistry = JSON.parse(spider.get(npc.world.getName() + "_NodeRegistry.json"));
+            if(start in nodeRegistry) // Check to see if starting node is in the Node Registry
             {
-                if(end in nodeRegistry)
+                if(end in nodeRegistry)// Check to see if ending node is in the Node Registry
                 {
-                    var nav = nodeRegistry[start]["Pos"]
-                    npc.navigateTo(nav[0], nav[1], nav[2], speed)
-                    // Reconstruct Path
-                    var lastNode = paths[start][end][1]
-                    if(lastNode)
+                    // Good to go, find the paths for start to end
+                    var lastNode = paths[start][end][1];
+                    // To reconstruct the list, it must be done in reverse
+                    if(lastNode) // Final check, if this fails: there's no path from start to end
                     {
-                        shortestPath = []
+                        shortestPath = [];
                         while(lastNode)
                         {
-                            //npc.world.broadcast("lastNode: " + JSON.stringify(lastNode))
-                            //npc.world.broadcast("shortestPath: " + JSON.stringify(shortestPath))
-                            shortestPath.push(lastNode)
-                            //npc.world.broadcast("shortestPath: " + JSON.stringify(shortestPath))
-                            lastNode = paths[start][lastNode][1]
+                            shortestPath.push(lastNode);
+                            lastNode = paths[start][lastNode][1];
                         }
-                        shortestPath.reverse()
-                        shortestPath.push(end)
+                        shortestPath.reverse();
+                        shortestPath.push(end);
                     }
-                    else
-                    {
-                        // Cannot reach destination
-                        npc.world.broadcast("[ERR!] " + npc.getDisplay().getName() + ": " + end + " Cannot be reached from " + start)
-                    }
-
+                    else{log("ERR!]: " + end + " is not reachale from " + start + "!");}
                 }
-                else
-                {
-                    npc.world.broadcast("[ERR!] " + npc.getDisplay().getName() + ": " + end + " is not in Node Registry!")
-                }
-                
-
+                else{log("[ERR!]: Ending node is not in the Node Registry, check spelling");}
             }
-            else
-            {
-                npc.world.broadcast("[ERR!] " + npc.getDisplay().getName() + ": " + start + " is not in Node Registry!")
-                
-            }
+            else{log("[ERR!]: Starting node is not in the Node Registry, check spelling");}
         }
-        else
-        {
-            npc.world.broadcast("[ERR!] " + npc.getDisplay().getName() + ": " + "Couldn't find mapped paths, please map paths for me to navigate!")
-        }
+        else{log("[ERR!]: Couldn't find mapped paths, please map paths for me to navigate!");}
     }
-    else
-    {
-        npc.world.broadcast("[ERR!] " + npc.getDisplay().getName() + ": " + "Couldn't find Node Registry, please create nodes and map paths for me to navigate!")
-    }
+    else{log("[ERR!]: Couldn't find Node Registry, please create nodes and map paths for me to navigate!");}
 
-    // Move to start
-    //npc.world.broadcast("PrepNav returning " + shortestPath)
-    return shortestPath
+    return shortestPath;
 }
 
-function tick(event)
+function trekTo(npc, destination, speed)
 {
-    if(event.npc.getStoreddata().get("CurrentLocation") != event.npc.getStoreddata().get("NavTo"))
-    {
-        //event.npc.world.broadcast("I need to navigate!")
-        if(event.npc.getStoreddata().get("navPath"))
-        {
-            // Already have a path made
-            //event.npc.world.broadcast("navPathExists!")
-            if(event.npc.getStoreddata().get("isNavigating"))
-            {
-                // NPC is trying to reach somewhere
-                if(!(event.npc.isNavigating()))
-                {
-                    // NPC has failed for some reason? Auto Recover
-                    //event.npc.world.broadcast("I'm trying to auto recover!")
-                    event.npc.clearNavigation()
-                    var spiderClass = Java.type("org.baito.forge.jsonified.Spider")
-                    var spider = new spiderClass()
-                    spider.in("CNPCsRoads")
-                    var nodeRegistry = JSON.parse(spider.get(event.npc.world.getName() + "_NodeRegistry.json"))
+    // Starts timer 1, asks for shortest path, moves npc to starting node. Basically start the whole thing
+    var shortestPath = getShortestPath(npc.getStoreddata().get("CurrentLocation"), destination, npc);
 
-                    var lastNodeReached = event.npc.getStoreddata().get("LastLocationRecorded")
-                    var navPath = JSON.parse(event.npc.getStoreddata().get("navPath"))
-                    var meInNavPath = navPath.indexOf(lastNodeReached)
-                    //var returnTo = nodeRegistry[currentLocation]["Pos"] // For going backwards, initial solutions are silly :P
-                    var nextNode = navPath[meInNavPath + 1]
-                    var nextNodePos = nodeRegistry[nextNode]["Pos"]
-                    var fails = event.npc.getStoreddata().get("consecutiveRevoverFails") // After 3 failed attempts to reach a node, the npc probably can't get to it
-                    if(fails < 3)
-                    {
-                        //event.npc.world.broadcast("Trying to autorecover to " + JSON.stringify(nextNodePos))
-                        //event.npc.navigateTo(returnTo[0], returnTo[1], returnTo[2], event.npc.getStoreddata().get("NavSpeed"))
-                        event.npc.navigateTo(nextNodePos[0], nextNodePos[1], nextNodePos[2], event.npc.getStoreddata().get("NavSpeed"))
-                        fails += 1
-                        event.npc.getStoreddata().put("consecutiveRevoverFails", fails)
-                    }
-                    else
-                    {
-                        event.npc.setPosition(nextNodePos[0], nextNodePos[1], nextNodePos[2])
-                    }
+    if(shortestPath)
+    {
+        // Shortest path came back, we have a path to follow
+        var spiderClass = Java.type("org.baito.forge.jsonified.Spider");
+        var spider = new spiderClass();
+        spider.in("CNPCsRoads");
+        var nodeRegistry = JSON.parse(spider.get(npc.world.getName() + "_NodeRegistry.json"));
+
+        npc.getStoreddata().put("NavSpeed", speed);
+        var start = nodeRegistry[npc.getStoreddata().get("CurrentLocation")];
+        var end = nodeRegistry[destination];
+        npc.getStoreddata().put("LastLocationRecorded", npc.getStoreddata().get("CurrentLocation"));
+        npc.getStoreddata().put("navPath", JSON.stringify(shortestPath));
+        npc.getStoreddata().put("NavTo", destination);
+        npc.getStoreddata().put("isNavigating", 1);
+
+        npc.clearNavigation();
+        npc.navigateTo(start["Pos"][0], start["Pos"][1], start["Pos"][2], speed);
+        npc.getTimers().stop(1); // Not sure why it's a problem
+        npc.getTimers().start(1, 1, true);
+        log("Navigation started " + start["Name"] + " to " + end["Name"])
+    }
+    else{log("[ERR!]: Did not get a shortest path, navigation not started!");}
+}
+
+function timer(event)
+{
+    if(event.id == 1)
+    {
+        if(!event.npc.isNavigating())
+        {
+            // NPC is standing still, make it move again if needed
+            if(event.npc.getStoreddata().get("CurrentLocation") != event.npc.getStoreddata().get("NavTo"))
+            {
+                // NPC needs to move to the next node or auto-recover
+                var spiderClass = Java.type("org.baito.forge.jsonified.Spider");
+                var spider = new spiderClass();
+                spider.in("CNPCsRoads");
+                var nodeRegistry = JSON.parse(spider.get(event.npc.world.getName() + "_NodeRegistry.json"));
+                var navPath = JSON.parse(event.npc.getStoreddata().get("navPath"));
+                var lastNodeReached = event.npc.getStoreddata().get("LastLocationRecorded");
+                var lastNodeInPath = navPath.indexOf(lastNodeReached);
+                var nextNode = navPath[lastNodeInPath + 1];
+                var nextNodePos = nodeRegistry[nextNode]["Pos"];
+                var fails = event.npc.getStoreddata().get("consecutiveRevoverFails"); // After 3 failed attempts to reach a node, the npc probably can't get to it
+                if(fails <= 3)
+                {
+                    //event.npc.world.broadcast("Trying to autorecover to " + JSON.stringify(nextNodePos))
+                    event.npc.navigateTo(nextNodePos[0], nextNodePos[1], nextNodePos[2], event.npc.getStoreddata().get("NavSpeed"));
+                    fails += 1;
+                    event.npc.getStoreddata().put("consecutiveRevoverFails", fails);
                 }
                 else
                 {
-                    //var fails = event.npc.getStoreddata().get("consecutiveRevoverFails")
-                    event.npc.getStoreddata().put("consecutiveRevoverFails", 0)
-
+                    //event.npc.world.broadcast("Teleporting..." + nextNodePos[0] + " " + nextNodePos[1] + " " + nextNodePos[2]);
+                    event.npc.setPosition(nextNodePos[0] + 0.5, nextNodePos[1], nextNodePos[2] + 0.5);
+                    event.npc.getStoreddata().put("consecutiveRevoverFails", 0);
                 }
-
             }
             else
             {
-                if(!(event.npc.isNavigating()))
-                {
-                    //event.npc.world.broadcast("Travel to Start complete")
-                    event.npc.getStoreddata().put("isNavigating", 1)
-                    //event.npc.world.broadcast("navPath in NPC: " + event.npc.getStoreddata().get("navPath"))
-                }
+                // Navigation finished
+                event.npc.getStoreddata().put("isNavigating", 0);
+                event.npc.getStoreddata().put("navPath", "");
+                event.npc.getStoreddata().put("isNavigating", 0);
+                event.npc.getTimers().stop(1);
+                log("Navigation Complete");
             }
         }
         else
         {
-            // No nav path exists, make one
-            //event.npc.world.broadcast("Navigating from: " + event.npc.getStoreddata().get("CurrentLocation") + " to " + event.npc.getStoreddata().get("NavTo"))
-            var shortestPath = prepNav(event.npc, event.npc.getStoreddata().get("CurrentLocation"), event.npc.getStoreddata().get("NavTo"), event.npc.getStoreddata().get("NavSpeed"))
-            //event.npc.world.broadcast("shortestPath after return: " + JSON.stringify(shortestPath))
-            event.npc.getStoreddata().put("LastLocationRecorded", event.npc.getStoreddata().get("CurrentLocation"))
-            event.npc.getStoreddata().put("navPath", JSON.stringify(shortestPath))
-            //event.npc.world.broadcast("navPath in NPC: " + event.npc.getStoreddata().get("navPath"))
+            event.npc.getStoreddata().put("consecutiveRevoverFails", 0);
         }
-        //event.npc.getStoreddata().put("LastLocationRecorded", event.npc.getStoreddata().get("CurrentLocation"))
-        //var shortestPath = prepNav(event.npc, event.npc.getStoreddata().get("CurrentLocation"), event.npc.getStoreddata().get("NavTo"), event.npc.getStoreddata().get("NavSpeed"))
-    }
-    else if(event.npc.getStoreddata().get("isNavigating"))
-    {
-        event.npc.getStoreddata().put("isNavigating", 0)
-        event.npc.getStoreddata().put("navPath", "")
     }
 }
